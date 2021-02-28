b6a4dc6c980ac37fff5b4c9c0dfa7527163f7597
Francesco Ciocchetti <primeroznl@gmail.com>
2021-02-28 19:11:04 +0100

    Search root for grub by label

--- grub-image.sh
+++ grub-image.sh
@@ -5,14 +5,14 @@ THIS_DIR=`pwd`
 TARGET_DIR=$THIS_DIR/root/img
 GRUB2_IMAGE=$THIS_DIR/grub.img
 #GRUB2_PREFIX='(hd0,msdos1)/boot/grub'
-GRUB2_PREFIX='(hd0,msdos1)/boot/grub'
+GRUB2_PREFIX='($root)/boot/grub'
 GRUB2_CFG=$TARGET_DIR/boot/grub/grub.cfg
 GRUB2_EARLY_CFG=$THIS_DIR/grub-early.cfg
 GRUB2_TUPLE=i386-pc
 GRUB2_TARGET=i386
 GRUB2_PLATFORM=pc
 # https://github.com/buildroot/buildroot/blob/master/boot/grub2/Config.in
-GRUB2_MODULES="at_keyboard configfile biosdisk ext2 linux test serial halt minicmd terminal cat acpi part_gpt part_msdos boot fat squash4 normal biosdisk"
+GRUB2_MODULES="at_keyboard configfile biosdisk ext2 linux test serial halt minicmd terminal cat acpi part_gpt part_msdos boot fat squash4 normal biosdisk search_fs_file search_label"
 
 cp /usr/lib/grub/i386-pc/boot.img .
 
@@ -20,6 +20,7 @@ cp /usr/lib/grub/i386-pc/boot.img .
 		-O $GRUB2_TUPLE \
 		-o $GRUB2_IMAGE \
 		-p "$GRUB2_PREFIX" \
+    -c $GRUB2_EARLY_CFG \
     $GRUB2_MODULES
     #-c $GRUB2_EARLY_CFG \
 		#-p "$GRUB2_PREFIX" \
